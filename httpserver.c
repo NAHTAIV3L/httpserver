2f45d511f8d0175c6561848d9785a653eb6291dd
NAHTAIV3L <rbeckettvt@gmail.com>
2023-05-05 10:20:05 -0400

    segfault and segint handler

--- httpserver.c
+++ httpserver.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
@@ -7,11 +8,50 @@
 #include <string.h>
 #include <unistd.h>
 
+int sfd;
+int cfd;
+
+void mysighandler()
+{
+    if (cfd >= 0)
+        close(cfd);
+    if (sfd >= 0)
+        close(sfd);
+    puts("");
+    exit(1);
+}
+
+void mysegfaulthandler()
+{
+    if (cfd >= 0)
+        close(cfd);
+    if (sfd >= 0)
+        close(sfd);
+    puts("segfault");
+    abort();
+}
+
+void SignalInit()
+{
+struct sigaction sigIntHandler;
+sigIntHandler.sa_handler = mysighandler;
+sigemptyset(&sigIntHandler.sa_mask);
+sigIntHandler.sa_flags = 0;
+
+struct sigaction sigSegfaultHandler;
+sigSegfaultHandler.sa_handler = mysegfaulthandler;
+sigemptyset(&sigSegfaultHandler.sa_mask);
+sigSegfaultHandler.sa_flags = 0;
+
+sigaction(SIGINT, &sigIntHandler, NULL);
+sigaction(SIGSEGV, &sigSegfaultHandler, NULL);
+}
+
 int gethandler(int cfd, char *filepath, char *msg)
 {
     char *part1 = strtok(msg, " ");
     char *path = strtok(NULL, " ");
-    char *part3 = strtok(NULL, " ");
+    char *part3 = strtok(NULL, "\n");
     char *file = NULL;
 
     int pathlength = strlen(path) + strlen(filepath);
@@ -55,7 +95,8 @@ int gethandler(int cfd, char *filepath, char *msg)
     }
     else
     {
-        char tosend[4096] = "HTTP/1.1 404 Not Found";
+        char tosend[4096];
+        snprintf(tosend, 4096, "%s 404 Not Found", part3);
         send(cfd, tosend, 4096-1, 0);
         perror("!file not found");
         return -1;
@@ -65,15 +106,17 @@ int gethandler(int cfd, char *filepath, char *msg)
     {
         // start to process your data / extract strings here...
         char tosend[4096];
-        snprintf(tosend, 4096, "HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length: %d\n\n%s", (int)length, buffer);
+        snprintf(tosend, 4096, "%s 200 OK\nContent-Type: text/html\nContent-Length: %d\n\n%s", part3, (int)length, buffer);
         send(cfd, tosend, 4096-1, 0);
     }
     else { return -1; }
+    free(file);
     return 0;
 }
 
 int main(int argc, char **argv)
 {
+    SignalInit();
     char filepath[1024];
     if (argc > 1)
     {
@@ -92,7 +135,7 @@ int main(int argc, char **argv)
     struct sockaddr clientinfo = {0};
     socklen_t clientinfolen = sizeof(clientinfo);
 
-    int sfd = socket(AF_INET, SOCK_STREAM, 0);
+    sfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sfd < 0) { perror("!socket"); return -1; }
 
     if (0 > bind(sfd, (struct sockaddr*)&serverinfo, serverinfolen))
@@ -109,7 +152,7 @@ int main(int argc, char **argv)
             return -1;
         }
 
-        int cfd = accept(sfd, &clientinfo, &clientinfolen);
+        cfd = accept(sfd, &clientinfo, &clientinfolen);
         if (0 > cfd) { perror("!accept"); return -1; }
 
         char msg[4096];
@@ -118,10 +161,24 @@ int main(int argc, char **argv)
         char temp[4096];
         strncpy(temp, msg, 4096);
 
-        if (0 > gethandler(cfd, filepath, temp))
-        { perror("!gethandler"); }
+        for (int i = 0; i < 4096; i++)
+        {
+            if (msg[i] == ' ')
+            { msg[i] = 0; break; }
+        }
+
+        if (strcmp(msg, "GET") == 0)
+        {
+            if (0 > gethandler(cfd, filepath, temp))
+            { perror("!gethandler"); }
+        }
+        else if (strcmp(msg, "POST") == 0)
+        {}
+        else if (strcmp(msg, "HEAD") == 0)
+        {}
 
         close(cfd);
     }
+    close(sfd);
     return 0;
 }
